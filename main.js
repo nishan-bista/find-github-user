c7737ce080cb2ab2bee01313ff19fa15c3f5b4f3
Nishan Bista <nishanbista525@gmail.com>
2024-03-29 20:53:36 +0545

    version 2.0

--- main.js
+++ main.js
@@ -1,191 +1,171 @@
-const inputFromUserBody = document.querySelector(".inputFromUser-body");
-const inputData = document.querySelector(".inputData");
-const findButton = document.querySelector(".findUserButton");
+const root = document.querySelector(".root"); // getting root body
 
-const info = document.querySelector(".info");
+const alertBar = document.createElement("div"); // creating alertbar
+alertBar.classList.add("alertBar");
+alertBar.classList.add("alertBar_h");
+root.append(alertBar);
 
-setTimeout(() => {
-  info.remove();
-}, 4000);
-const alertBox = document.querySelector(".alertBox");
-findButton.addEventListener("click", () => {
-  if (inputData.value === "") {
-    alertBox.style.padding = "10px";
-    alertBox.textContent = "Alert : Input is empty.";
+const searchBody = document.createElement("div");
+searchBody.classList.add("searchBody");
+root.appendChild(searchBody);
 
+/**
+ *
+ * Functions and protoypes
+ *
+ */
+
+//prototype for string that capitalizes
+String.prototype.capitalize = function () {
+  return this[0].toUpperCase() + this.slice(1);
+};
+
+//function to give alert messages
+function alertMessage(mes) {
+  if (root.firstChild.classList != "alertBar") {
+    alertBar.classList.remove("alertBar_h");
+    alertBar.textContent = `Alert: ${mes}`;
     setTimeout(() => {
-      alertBox.style.padding = "0";
-      alertBox.textContent = "";
-    }, 2000);
+      alertBar.classList.add("alertBar_h");
+    }, 3000);
+    alertBar.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "start",
+    });
+  }
+}
 
+const inputSearchBox = document.createElement("input");
+inputSearchBox.classList.add("inputSearchBox");
+inputSearchBox.setAttribute("placeholder", "nishan812");
+searchBody.appendChild(inputSearchBox);
+
+const searchButton = document.createElement("button");
+searchButton.classList.add("searchButton");
+searchButton.textContent = "Search";
+searchBody.appendChild(searchButton);
+
+searchButton.addEventListener("click", () => {
+  const inputByUser = inputSearchBox.value.trim().toLowerCase();
+  if (inputByUser == "") {
+    alertMessage("username is empty");
   } else {
-    inputFromUserBody.remove();
-    const body = document.querySelector(".allBody");
+    searchButton.disabled = true;
+    fetch(`https://api.github.com/users/${inputByUser}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message == "Not Found") {
+          alertMessage("User doesnot exist");
+          searchButton.disabled = false;
+        } else {
+          inputSearchBox.value = "";
+          searchBody.style.display = "none";
+          const card = document.createElement("div");
+          card.classList.add("card");
+          root.appendChild(card);
 
-    fetch(`https://api.github.com/users/${inputData.value}`)
-      .then((response) => response.json())
-      .then((result) => {
-        //creating main container
-        const container = document.createElement("div");
-        container.classList.add("container");
-        body.appendChild(container);
+          const imageSection = document.createElement("div");
+          imageSection.classList.add("imageSection");
+          card.appendChild(imageSection);
 
-        //image section
-        const image = document.createElement("div");
-        image.classList.add("imageContainer");
-        image.style.backgroundImage = `url(${result.avatar_url})`;
-        container.appendChild(image);
+          const image = document.createElement("div");
+          image.classList.add("image");
+          image.style.backgroundImage = `url(${data.avatar_url})`;
+          imageSection.appendChild(image);
 
-        // info container
-        const infoContainer = document.createElement("div");
-        infoContainer.classList.add("infoContainer");
-        container.appendChild(infoContainer);
+          const infoSectionForCard = document.createElement("div");
+          infoSectionForCard.classList.add("infoSectionForCard");
+          card.appendChild(infoSectionForCard);
 
-        //name
-        const userName = document.createElement("div");
-        userName.classList.add("username");
-        userName.innerHTML = `<p>Name : ${result.name}</p>`;
-        infoContainer.appendChild(userName);
+          const dataToDisplayInCard = {
+            name: data.name,
+            email: data.email,
+            company: data.company,
+            type: data.type,
+          };
 
-        //user id
-        const userId = document.createElement("div");
-        userId.classList.add("userId");
-        userId.innerHTML = `<p>Id : ${result.id}</p>`;
-        infoContainer.appendChild(userId);
-
-        //  email
-        const userEmail = document.createElement("div");
-        userEmail.classList.add("useremail");
-        userEmail.innerHTML = `<p>Email : ${result.email}</p>`;
-        infoContainer.appendChild(userEmail);
-
-        //followers
-        const followers = document.createElement("div");
-        followers.classList.add("followers");
-        followers.innerHTML = `<p>Followers : ${result.followers}</p>`;
-        infoContainer.appendChild(followers);
-
-        //following
-        const following = document.createElement("div");
-        following.classList.add("following");
-        following.innerHTML = `<p>Following : ${result.following}</p>`;
-        infoContainer.appendChild(following);
-
-        //container for button
-        const buttonContainer = document.createElement("div");
-        buttonContainer.classList.add("buttonContainer");
-        container.appendChild(buttonContainer);
-
-        //button for show more info
-        const showMoreButton = document.createElement("button");
-        showMoreButton.setAttribute("type", "submit");
-        showMoreButton.textContent = "Show all info";
-        buttonContainer.appendChild(showMoreButton);
-
-        //button for search another user
-        const searchAnotherUser = document.createElement("button");
-        searchAnotherUser.textContent = "Search another User";
-        buttonContainer.appendChild(searchAnotherUser);
-
-        //event listener for search another user
-        searchAnotherUser.addEventListener("click", () => {
-          location.reload();
-        });
-
-        //event listener for  show more button
-
-        showMoreButton.addEventListener("click", () => {
-          if (container.classList.contains("moreInfoActive")) {
-            alert("Already displayed");
-          } else {
-            body.style.marginTop = "200px";
-            container.classList.add("moreInfoActive");
-            const moreInfo = document.createElement("div");
-            moreInfo.classList.add("moreInfoContainer");
-            body.appendChild(moreInfo);
-
-            //more data into show more info
-
-            const bio = document.createElement("span");
-            bio.textContent = `Bio : "${result.bio}"`;
-            moreInfo.appendChild(bio);
-
-            //username
-            const username = document.createElement("span");
-            username.textContent = `Username : ${result.name}`;
-            moreInfo.appendChild(username);
-
-            //user id
-            const userId = document.createElement("span");
-            userId.textContent = `User Id : ${result.login}`;
-            moreInfo.appendChild(userId);
-
-            //usertype
-            const type = document.createElement("span");
-            type.textContent = `Account Type : ${result.type}`;
-            moreInfo.appendChild(type);
-
-            //location
-            const location = document.createElement("span");
-            location.textContent = `Location : ${result.location}`;
-            moreInfo.appendChild(location);
-
-            //email
-            const email = document.createElement("span");
-            email.textContent = `Email : ${result.email}`;
-            moreInfo.appendChild(email);
-
-            //company
-            const company = document.createElement("span");
-            company.classList.add("company");
-            company.innerHTML = `<p>Company : ${result.company}</p>`;
-            moreInfo.appendChild(company);
-
-            //hireable
-            const hireable = document.createElement("span");
-            hireable.textContent = `Hireable : ${result.hireable}`;
-            moreInfo.appendChild(hireable);
-
-            //Public repos
-            const publicRepos = document.createElement("span");
-            publicRepos.textContent = `Public Repository : ${result.public_repos}`;
-            moreInfo.appendChild(publicRepos);
-
-            //followers and following
-            moreInfo.appendChild(followers);
-            moreInfo.appendChild(following);
-
-            //for twitter
-            const twitter = document.createElement("span");
-            twitter.textContent = `Twitter username : ${result.twitter_username}`;
-            moreInfo.appendChild(twitter);
-
-            //account creation 
-            const accountCreatedDate = document.createElement("span");
-            accountCreatedDate.textContent = `Account created at: ${result.created_at}`;
-            moreInfo.appendChild(accountCreatedDate);
-
-
-            //latest profile update
-            const accountUpdatedDate = document.createElement("span");
-            accountUpdatedDate.textContent = `Updated at : ${result.updated_at}`;
-            moreInfo.appendChild(accountUpdatedDate);
-
-            //blogs
-            const blog = document.createElement("span");
-            blog.innerHTML = ` Blog : <a href="${result.blog}">${result.blog}</a>`;
-            moreInfo.appendChild(blog);
-
-            const gotoAccount = document.createElement("a");
-            gotoAccount.href = result.html_url;
-            gotoAccount.textContent =
-              "Click here to goto user account in Github";
-            moreInfo.appendChild(gotoAccount);
+          for (let key in dataToDisplayInCard) {
+            const x = document.createElement("div");
+            x.textContent = `${key.capitalize()}: ${
+              dataToDisplayInCard[key] == null
+                ? (textContent = "Not Available!")
+                : dataToDisplayInCard[key]
+            }`;
+            infoSectionForCard.appendChild(x);
           }
-        });
-      })
-      .catch(() => {
-        alert("User Not Found!");
+
+          const buttonBodyOnCard = document.createElement("div");
+          buttonBodyOnCard.classList.add("buttonBodyOnCard");
+          card.appendChild(buttonBodyOnCard);
+
+          const showFullDetailsButton = document.createElement("button");
+          showFullDetailsButton.textContent = "Show Full detail";
+          showFullDetailsButton.classList.add("showFullDetailsButton");
+          buttonBodyOnCard.appendChild(showFullDetailsButton);
+
+          const searchAnotherUserButton = document.createElement("button");
+          searchAnotherUserButton.classList.add("searchAnotherUserButton");
+          searchAnotherUserButton.textContent = "Search Another User";
+          buttonBodyOnCard.appendChild(searchAnotherUserButton);
+
+          //event for button show full details
+          showFullDetailsButton.addEventListener("click", () => {
+            if (root.lastChild.classList == "fullDeatailsBody") {
+              alertMessage("Already displayed");
+            } else {
+              const fullDeatailsBody = document.createElement("div");
+              fullDeatailsBody.classList.add("fullDeatailsBody");
+              root.appendChild(fullDeatailsBody);
+              fullDeatailsBody.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+                inline: "start",
+              });
+
+              const full_dataToDisplay = {
+                type: data.type,
+                name: data.name,
+                username: data.login,
+                id: data.id,
+                bio: data.bio,
+                company: data.company,
+                location: data.location,
+                email: data.email,
+                twitter: data.twitter_username,
+                hireable: data.hireable,
+                followers: data.followers,
+                following: data.following,
+                public_repos: data.public_repos,
+                blog: data.blog,
+                createdAt: data.created_at,
+                updatedAt: data.updated_at,
+                gotoProfile: data.html_url,
+              };
+
+              for (let key in full_dataToDisplay) {
+                const item = document.createElement("div");
+                item.innerHTML = `${key.capitalize()}: ${
+                  full_dataToDisplay[key] == null
+                    ? "Not Availabel !"
+                    : full_dataToDisplay[key] == ""
+                    ? "Not Found !"
+                    : key == "blog" || key == "gotoProfile"
+                    ? `<a href=${full_dataToDisplay[key]}>${full_dataToDisplay[key]}</a>`
+                    : full_dataToDisplay[key]
+                }`;
+
+                fullDeatailsBody.append(item);
+                item.style.display = "inline-block";
+              }
+            }
+          });
+
+          //event fot button search another user
+          searchAnotherUserButton.addEventListener("click", () => {
+            location.reload();
+          });
+        }
       });
   }
 });
